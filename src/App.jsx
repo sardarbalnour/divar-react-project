ae043c4d6a7b021fa2338be0ceabe363b2708efe
Sardar balnour <balnour.sardar@gmail.com>
2026-08-17 12:10:49 +0330

    React Query configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Router from "./router/Router";
+import defaultOptions from "./configs/reactQueryConfigs";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
