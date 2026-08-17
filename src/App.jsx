51e83c576fd6eaf6e7cc85df38ef69d5d36009c5
Sardar balnour <balnour.sardar@gmail.com>
2026-08-17 11:49:40 +0330

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import AuthPage from "./pages/AuthPage";
+import { BrowserRouter } from "react-router-dom";
+
+import Router from "./router/Router";
 
 function App() {
   return (
-    <div>
-      <AuthPage />
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,5 +1,5 @@
 import { checkOtp } from "../../services/auth";
-import setCookie from "../../utils/cookie";
+import {setCookie} from "../../utils/cookie";
 
 function CheckOtpForm({ code, setCode, mobile, setStep }) {
   const submitHandler = async (event) => {
