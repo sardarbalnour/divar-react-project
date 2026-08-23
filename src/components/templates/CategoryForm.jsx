46a0d63b2fbfc14f5b69ced1561b586f19ba1b32
Sardar balnour <balnour.sardar@gmail.com>
2026-08-23 16:13:03 +0330

    post category data to back-end

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+
+import { addCategory } from "../../services/admin";
 
 import styles from "./CategoryForm.module.css";
 
 function CategoryForm() {
   const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+  const [showMessage, setShowMessage] = useState(false);
+
+  const { mutate, isPending, error, data } = useMutation({
+    mutationFn: addCategory,
+  });
+  console.log({ isPending, error, data });
+
+  useEffect(() => {
+    if (!error && !data) return;
+
+    setShowMessage(true);
+    const timer = setTimeout(() => setShowMessage(false), 3000);
+
+    return () => clearTimeout(timer);
+  }, [error, data]);
 
   const changeHandler = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
@@ -11,7 +29,12 @@ function CategoryForm() {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(form);
+
+    if (!form.name || !form.slug || !form.icon) return;
+
+    mutate(form);
+
+    // console.log(form);
   };
 
   return (
@@ -21,6 +44,9 @@ function CategoryForm() {
       className={styles.form}
     >
       <h3>دسته بندی جدید</h3>
+      {showMessage && error && <p>{error.message}</p>}
+      {isPending && <p>در حال ارسال...</p>}
+      {showMessage && data && <p>دسته بندی با موفقیت ایجاد شد.</p>}
 
       <label htmlFor="name">اسم دسته بندی</label>
       <input type="text" name="name" id="name" />
@@ -31,7 +57,9 @@ function CategoryForm() {
       <label htmlFor="icon">آیکون</label>
       <input type="text" name="icon" id="icon" />
 
-      <button type="submit">ایجاد</button>
+      <button type="submit" disabled={isPending}>
+        ایجاد
+      </button>
     </form>
   );
 }
