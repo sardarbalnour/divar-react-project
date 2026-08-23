99705787fbc931ae9e3762f271c3915fb7dcdc06
Sardar balnour <balnour.sardar@gmail.com>
2026-08-23 11:36:02 +0330

    CategoryForm

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,8 +1,11 @@
+import CategoryForm from "../components/templates/CategoryForm";
 
 function AdminPage() {
   return (
-    <div>AdminPage</div>
-  )
+    <div>
+      <CategoryForm />
+    </div>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
