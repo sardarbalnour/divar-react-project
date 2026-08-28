2a99bd5bc13abe889dd57294a2e0d8b47a75a52e
Sardar balnour <balnour.sardar@gmail.com>
2026-08-29 00:54:07 +0330

    post list style

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -4,6 +4,8 @@ import { getPosts } from "../../services/user";
 import Loader from "../modules/Loader";
 import { sp } from "../../utils/numbers";
 
+import styles from "./PostList.module.css";
+
 function PostList() {
   const { data, isPending } = useQuery({
     queryKey: ["my-post-list"],
@@ -16,16 +18,16 @@ function PostList() {
       {isPending ? (
         <Loader />
       ) : (
-        <div>
+        <div className={styles.list}>
           <h3>آگهی های شما</h3>
           {data.data.posts.map((post) => (
-            <div key={post._id}>
+            <div key={post._id} className={styles.post}>
               <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
               <div>
                 <p>{post.options.title}</p>
                 <span>{post.options.content}</span>
               </div>
-              <div>
+              <div className={styles.price}>
                 <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
                 <span>{sp(post.amount)} تومان</span>
               </div>
