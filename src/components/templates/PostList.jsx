ab31d66103b5dad0f0badefbc5fd8b023f4df0c2
Sardar balnour <balnour.sardar@gmail.com>
2026-08-28 17:00:49 +0330

    show posts

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getPosts } from "../../services/user";
+import Loader from "../modules/Loader";
 
 function PostList() {
   const { data, isPending } = useQuery({
@@ -9,7 +10,30 @@ function PostList() {
   });
   console.log(data, isPending);
 
-  return <div>PostList</div>;
+  return (
+    <div>
+      {isPending ? (
+        <Loader />
+      ) : (
+        <div>
+          <h3>آگهی های شما</h3>
+          {data.data.posts.map((post) => (
+            <div key={post._id}>
+              <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
+              <div>
+                <p>{post.options.title}</p>
+                <span>{post.options.content}</span>
+              </div>
+              <div>
+                <p>{post.createdAt}</p>
+                <span>{post.amount} تومان</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PostList;
