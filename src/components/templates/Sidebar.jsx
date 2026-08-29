e8f736a8d59a1abb8eb968a71be7e57c44dc600c
Sardar balnour <balnour.sardar@gmail.com>
2026-08-29 17:13:40 +0330

    Sidebar

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+
+import { getCategory } from "../../services/admin";
 
 function Sidebar() {
+  const { data } = useQuery({
+    queryKey: ["get-categories"],
+    queryFn: getCategory,
+  });
+  console.log(data);
+
   return (
-    <div>Sidebar</div>
-  )
+    <div>
+      <h4>دسته بندی</h4>
+      <ul>
+        {data?.data.map((i) => (
+          <li key={i._id}>
+            <img src={`${i.icon}.svg`} />
+            <p>{i.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
